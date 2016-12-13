18d2becaa31363ad7bf04d651b8f0e116a39f346
Dan Persa <dan.persa@gmail.com>
2016-12-13 10:21:31 +0100

    Fix the project file license; Remove the project group

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject org.clojars.danpersa/cats.match "0.1.1-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Apache License"
-            :url  "http://www.apache.org/licenses/LICENSE-2.0"}
+(defproject cats.match "0.1.1-SNAPSHOT"
+  :description "cats.match provides pattern matching for the monads in the clojure cats library."
+  :url "https://github.com/zalando/cats.match"
+  :license {:name "MIT License"
+            :url  "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure       "1.8.0"        :scope "provided"]
                  [funcool/cats              "2.0.0"        :scope "provided"]
                  [org.clojure/core.match    "0.3.0-alpha4" :scope "provided"]]
   :profiles {:uberjar {:aot       :all
                        :jvm-opts ^:replace ["-Dclojure.compiler.direct-linking=true"]}
              :dev     {:plugins      [[lein-midje                  "3.2"]]
-                       :dependencies [[midje                       "1.9.0-alpha3"]]}})
+                       :dependencies [[midje                       "1.9.0-alpha6"]]}})
